9faba7e1b72a369fe12ab4262a9b48dd400c2637
Murali <muralivvrsn75683@gmail.com>
2022-08-20 22:10:42 +0530

    lsjf

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const background_change = document.getElementById('background-body');
 const colorChange = document.getElementById('change-color');
 const button = document.getElementById('btn');
+const shadow = document.getElementById('header');
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 function HexColor() {
     let hexColor = "#";
